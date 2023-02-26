43d78213622450a5dc1ba6804ce5e499172a4c7d
BbefWebsolutions <bbef.websolutions@gmail.com>
2023-02-25 23:39:30 -0300

    add apps to settings

--- ges/ges/settings.py
+++ ges/ges/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'appAlmacen',
+    'appCliente',
+    'appPedido',
+    'appUsuario',
 ]
 
 MIDDLEWARE = [
